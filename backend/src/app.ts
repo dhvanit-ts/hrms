ea88e518102eb6968a465440365fcdca0895526b
Dhvanit <dhvanit@techysquad.com>
2025-11-24 15:48:44 +0530

    added io logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import { env } from "@/common/config/env";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import { Server } from "socket.io";
 import { SocketService } from "@/services/socket.service";
 import http from "node:http";
 // import path from "node:path";
@@ -15,13 +14,7 @@ const socketService = new SocketService();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: env.ACCESS_CONTROL_ORIGIN,
-    methods: ["GET", "POST"],
-  },
-  transports: ["websocket"],
-});
+const io = socketService.createAndListenSocket(server);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -32,7 +25,6 @@ applySecurity(app);
 // app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // move these middlewares in route file after file upload middleware if you are using multer for file uploads
 
-socketService.listenSocket(io);
 registerRoutes(app);
 
 if (env.NODE_ENV !== "production") {
--- backend/src/services/socket.service.ts
+++ backend/src/services/socket.service.ts
@@ -1,26 +1,29 @@
-import { DefaultEventsMap, Server, Socket } from "socket.io";
-
-type SocketType = Socket<
-  DefaultEventsMap,
-  DefaultEventsMap,
-  DefaultEventsMap,
-  unknown
->;
+import { Server as SocketServer, Socket } from "socket.io";
+import { Server as HttpServer } from "node:http";
+import { env } from "@/common/config/env";
 
 class SocketService {
-  private handleSocketError = (socket: SocketType, error: unknown) => {
+  private handleSocketError = (socket: Socket, error: unknown) => {
     console.error(`Socket ${socket.id} error:`, error);
     if (error instanceof Error && "code" in error) {
       socket.emit("operation-error", {
-        code: error.code || "GENERIC_ERROR",
+        code: error?.code || "GENERIC_ERROR",
         message: error.message,
       });
     }
   };
 
-  listenSocket = (
-    io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
-  ) => {
+  createServer = (server: HttpServer) => {
+    return new SocketServer(server, {
+      cors: {
+        origin: env.ACCESS_CONTROL_ORIGIN,
+        methods: ["GET", "POST"],
+      },
+      transports: ["websocket"],
+    });
+  };
+
+  listenSocket = (io: SocketServer) => {
     io.on("connection", (socket) => {
       // your socket logic here
 
@@ -33,6 +36,12 @@ class SocketService {
         }
       });
     });
+  };
+
+  createAndListenSocket = (server: HttpServer) => {
+    const io = this.createServer(server);
+    this.listenSocket(io);
+    return io;
   };
 }
 

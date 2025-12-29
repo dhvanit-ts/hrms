314e2e8a75dc4bc630902de5f40f5f6c6f045c95
Dhvanit <dhvanit@techysquad.com>
2025-12-29 12:27:24 +0530

    fix: done

--- backie/src/core/middlewares/index.ts
+++ backie/src/core/middlewares/index.ts
@@ -4,6 +4,6 @@ export { requireRole } from "./requireRoles.middleware";
 export { default as errorHandlers } from "./error.middleware";
 export { default as rateLimit } from "./rate-limit.middleware";
 export { multipartUpload } from "./upload.middleware";
-export { validateSchema } from "./validate.middleware";
+export { validateRequest } from "./validate.middleware";
 
 export * as pipelines from "./pipelines";
